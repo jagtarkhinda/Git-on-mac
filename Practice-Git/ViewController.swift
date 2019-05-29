83bc8c253ff37bacf2e55243d7cf36bb6ff4d5cb
Jagtar Singh <jsk@jagtars-mac.local>
2019-05-29 17:34:48 -0400

    Initial Commit

---
+++ Practice-Git/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Practice-Git
+//
+//  Created by Jagtar Singh on 2019-05-29.
+//  Copyright © 2019 Jagtar Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
